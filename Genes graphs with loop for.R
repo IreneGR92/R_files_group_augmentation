fb432356c2ef5b1689b0303500060959b4e91d6c
IreneGR92 <42998147+IreneGR92@users.noreply.github.com>
2019-04-04 12:39:57 +0200

    Added print survival + last gen graph

--- Genes graphs with loop for.R
+++ Genes graphs with loop for.R
@@ -43,7 +43,7 @@ GA2 <- subset(GA2, age>0)
 setDF(GA2)
 
 
-#head(Parameters)
+#head(GA)
 #str(GA)
 #dim(files_main)
 #View(GA)
@@ -100,6 +100,7 @@ meanAge<-do_mean_LG(GA$Age)
 meanGroupSize<-do_mean_LG(GA$Group_size)
 meanHelp<-do_mean_LG(GA$meanHelp)
 meanDispersal<-do_mean_LG(GA$meanDispersal)
+meanSurvival<-do_mean_LG(GA$meanSurvival)
 meanRelatedness<-do_mean_LG(GA$Relatedness)
 meanCorr_Help_Disp<-do_mean_LG(GA$corr_Help_Disp)
 meanPropFloatBreeder<-do_mean_LG(GA$propFloatBreeder)
@@ -113,6 +114,7 @@ SD_Age<-do_SD_LG(GA$Age)
 SD_GroupSize<-do_SD_LG(GA$Group_size)
 SD_Help<-do_SD_LG(GA$meanHelp)
 SD_Dispersal<-do_SD_LG(GA$meanDispersal)
+SD_Survival<-do_SD_LG(GA$meanSurvival)
 SD_Relatedness<-do_SD_LG(GA$Relatedness)
 SDcorr_Help_Disp<-do_SD_LG(GA$corr_Help_Disp)
 SD_PropFloatBreeder<-do_SD_LG(GA$propFloatBreeder)
@@ -121,24 +123,24 @@ SD_PropFloatBreeder<-do_SD_LG(GA$propFloatBreeder)
 
 descriptives <- data.frame(Variable=c("alpha", "alphaAge", "alphaAge2",
                                       "beta", "betaAge", "age", "Group_size",
-                                      "Help","Dispersal", "Relatedness",
+                                      "Help","Dispersal", "Survival", "Relatedness",
                                       "Help_Disp", "propFloaterB"),
                            #A=c("=","=","=","=","=","=","=","=","=","=","=","="),
                            Mean=c(meanAlpha, meanAlphaAge, meanAlphaAge2,
                                   meanBeta, meanBetaAge, meanAge,meanGroupSize,
-                                  meanHelp,meanDispersal,meanRelatedness,
+                                  meanHelp,meanDispersal,meanSurvival,meanRelatedness,
                                   meanCorr_Help_Disp, meanPropFloatBreeder),
                            #B=c("±","±","±","±","±","±","±","±","±","±","±","±"),
                            SD=c(SD_Alpha,SD_AlphaAge,SD_AlphaAge2,
                                 SD_Beta,SD_BetaAge,SD_Age, SD_GroupSize,
-                                SD_Help,SD_Dispersal,SD_Relatedness,
+                                SD_Help,SD_Dispersal,SD_Survival,SD_Relatedness,
                                 SDcorr_Help_Disp, SD_PropFloatBreeder))
 
-descriptives2 <- data.frame(Variable=c( "Help","Dispersal", "Relatedness",
+descriptives2 <- data.frame(Variable=c( "Help","Dispersal", "Survival","Relatedness",
                                         "Group_size","Help_Disp"),
-                           Mean=c(meanHelp, meanDispersal, meanRelatedness,
+                           Mean=c(meanHelp, meanDispersal,meanSurvival, meanRelatedness,
                                   meanGroupSize, meanCorr_Help_Disp),
-                           SD=c(SD_Help,SD_Dispersal,SD_Relatedness,
+                           SD=c(SD_Help,SD_Dispersal,SD_Survival,SD_Relatedness,
                                 SD_GroupSize, SDcorr_Help_Disp))
 
 
@@ -150,43 +152,43 @@ write.xlsx(descriptives, paste(directory, "results_", nameFile, ".xlsx",sep=""),
 
 ###################### FORMULAS LAST GENERATION #################################
 
-# 
-# replace_with_zero_if_below_zero <- function(x) {
-#   x <- ifelse(x<0,0,x)
-#   return(x)
-# }
-# 
-# ###Help
-# 
-# help_Formula<-function(){
-#   help <- GA2$alpha+GA2$alphaAge*GA2$age+GA2$alphaAge2*GA2$age*GA2$age
-#   help <- sapply(help, replace_with_zero_if_below_zero)
-#   return(help)}
-# 
-# GA2$Help <- help_Formula()
-# GA2[GA2$type==0,]$Help<-NA
-# 
-# 
-# ###Dispersal
-# dispersal_Formula<-function(){
-#   dispersal<-1 / (1 + exp(GA2$betaAge*GA2$age - GA2$beta))
-#   return(dispersal)}
-# 
-# GA2$Dispersal<-dispersal_Formula()
-# GA2[GA2$type==0,]$Dispersal<-NA
-# 
-# ##Age
-# mean_age<-mean(GA2$age)
-# 
-# 
-# dichotonomic_age <- function(x) {
-#   x <- ifelse(x<mean_age,"< mean age","> mean age")
-#   return(x)
-# }
-# 
-# GA2$AgeDic<-dichotonomic_age(GA2$age)
-# GA2$AgeDic<-as.factor(GA2$AgeDic)
-# 
+
+replace_with_zero_if_below_zero <- function(x) {
+  x <- ifelse(x<0,0,x)
+  return(x)
+}
+
+###Help
+
+help_Formula<-function(){
+  help <- GA2$alpha+GA2$alphaAge*GA2$age+GA2$alphaAge2*GA2$age*GA2$age
+  help <- sapply(help, replace_with_zero_if_below_zero)
+  return(help)}
+
+GA2$Help <- help_Formula()
+GA2[GA2$type==0,]$Help<-NA
+
+
+###Dispersal
+dispersal_Formula<-function(){
+  dispersal<-1 / (1 + exp(GA2$betaAge*GA2$age - GA2$beta))
+  return(dispersal)}
+
+GA2$Dispersal<-dispersal_Formula()
+GA2[GA2$type==0,]$Dispersal<-NA
+
+##Age
+mean_age<-mean(GA2$age)
+
+
+dichotonomic_age <- function(x) {
+  x <- ifelse(x<mean_age,"< mean age","> mean age")
+  return(x)
+}
+
+GA2$AgeDic<-dichotonomic_age(GA2$age)
+GA2$AgeDic<-as.factor(GA2$AgeDic)
+
 
 
 
@@ -217,20 +219,19 @@ p2<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$meanDispersal)) +
   xlab("Generation")+ ylab("Dispersal")+
   coord_cartesian(ylim = c(0.049, 1))
 
+##Survival
+p3<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$meanSurvival)) +
+  geom_ribbon(aes(ymin=GA_means$meanSurvival-GA_SD$meanSurvival, ymax=GA_means$meanSurvival+GA_SD$meanSurvival),
+              alpha=0.3) +
+  geom_line(color="black", size=1)+
+  xlab("Generation")+ ylab("Survival")
+
 ##Relatedness plot
-p3<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$Relatedness)) +
+p4<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$Relatedness)) +
   geom_ribbon(aes(ymin=GA_means$Relatedness-GA_SD$Relatedness, ymax=GA_means$Relatedness+GA_SD$Relatedness),
               alpha=0.3) +
   geom_line(color="orange", size=1)+
   xlab("Generation")+ ylab("Relatedness")+
-  coord_cartesian(ylim = c(0.049, 1))
-
-##Proportion of floaters that become breeders compared to helpers plot
-p4<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$propFloatBreeder)) +
-  geom_ribbon(aes(ymin=GA_means$propFloatBreeder-GA_SD$propFloatBreeder, ymax=GA_means$propFloatBreeder+GA_SD$propFloatBreeder),
-              alpha=0.3) +
-  geom_line(color="green", size=1)+
-  xlab("Generation")+ ylab("Prop. floaters->breeders")+
   coord_cartesian(ylim = c(0.049, 1))
 
 
@@ -246,11 +247,21 @@ p5<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$Group_size)) +
 p6<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$meanCumHelp)) +
   geom_ribbon(aes(ymin=GA_means$meanCumHelp-GA_SD$meanCumHelp, ymax=GA_means$meanCumHelp+GA_SD$meanCumHelp),
               alpha=0.3) +
-  geom_line(color="black", size=1)+
+  geom_line(color="red", size=1)+
   xlab("Generation")+ ylab("Cummulative help")
 
+##Proportion of floaters that become breeders compared to helpers plot
+p7<-ggplot(GA_means, aes(x=GA_means$Generation, y=GA_means$propFloatBreeder)) +
+  geom_ribbon(aes(ymin=GA_means$propFloatBreeder-GA_SD$propFloatBreeder, ymax=GA_means$propFloatBreeder+GA_SD$propFloatBreeder),
+              alpha=0.3) +
+  geom_line(color="green", size=1)+
+  xlab("Generation")+ ylab("Prop. floaters->breeders")+
+  coord_cartesian(ylim = c(0.049, 1))
 
-grid.arrange(p1, p2, p6, p4, p3, p5, nrow = 3)
+
+
+
+grid.arrange(p1, p2, p6, p4, p5, p3, nrow = 3)
 
 
 ########### REACTION NORMS ####################
@@ -286,8 +297,8 @@ dispersalP<-plot(age,dispersal, type="l", col="blue", lwd=3, xlab="Age", ylab="D
 ########## LAST GENRATION ##########
 
 
-#plot(GA2$Help, GA2$Dispersal, col=GA2$replica,  xlab="Help", ylab="Dispersal")
-#title("Dispersal vs Help")
+plot(GA2$Help, GA2$Dispersal, col=GA2$replica,  xlab="Help", ylab="Dispersal")
+title("Dispersal vs Help")
 
 #plot(GA2$Help, GA2$Dispersal, col=c("blue","green")[GA2$AgeDic],  xlab="Help", ylab="Dispersal")
 #legend(x="bottomright", legend = levels(GA2$AgeDic), col=c("blue","green"), pch=1)
@@ -303,8 +314,4 @@ dev.off() # Close the pdf file
 }
 
 #########################################################################################################################################
-
-
-
-
 
