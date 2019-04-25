1b98ad08760077b9b054dabe5b49837b87ffbce6
IreneGR92 <42998147+IreneGR92@users.noreply.github.com>
2019-04-25 12:19:30 +0200

    added dispersal and relatedness analysis

--- ANOVA.R
+++ ANOVA.R
@@ -18,11 +18,108 @@ results$Bias<-as.factor(results$Bias)
 #Means and summary statistics by group
 
 library(Rmisc)
+library(lattice)
+library(plyr)
 
-sum <- summarySE(results, 
+#Help
+sumAll_H <- summarySE(results, 
                 measurevar="Help", 
                 groupvars=c("X0","Xh","Xn","K1","Bias"))
-sum
+
+sumX0_H <- summarySE(results, 
+                 measurevar="Help", 
+                 groupvars=c("X0"))
+
+sumXh_H <- summarySE(results, 
+                 measurevar="Help", 
+                 groupvars=c("Xh"))
+
+sumXn_H <- summarySE(results, 
+                 measurevar="Help", 
+                 groupvars=c("Xn"))
+
+sumK1_H <- summarySE(results, 
+                 measurevar="Help", 
+                 groupvars=c("K1"))
+
+sumBias_H <- summarySE(results, 
+                 measurevar="Help", 
+                 groupvars=c("Bias"))
+
+sumAll
+sumX0
+sumXh
+sumXn
+sumK1
+sumBias
+
+
+
+#Dispersal
+
+sumAll_D <- summarySE(results, 
+                      measurevar="Dispersal", 
+                      groupvars=c("X0","Xh","Xn","K1","Bias"))
+
+sumX0_D <- summarySE(results, 
+                     measurevar="Dispersal", 
+                     groupvars=c("X0"))
+
+sumXh_D <- summarySE(results, 
+                     measurevar="Dispersal", 
+                     groupvars=c("Xh"))
+
+sumXn_D <- summarySE(results, 
+                     measurevar="Dispersal", 
+                     groupvars=c("Xn"))
+
+sumK1_D <- summarySE(results, 
+                     measurevar="Dispersal", 
+                     groupvars=c("K1"))
+
+sumBias_D <- summarySE(results, 
+                       measurevar="Dispersal", 
+                       groupvars=c("Bias"))
+sumAll_D
+sumX0_D
+sumXh_D
+sumXn_D
+sumK1_D
+sumBias_D
+
+
+#Relatedness
+sumAll_R <- summarySE(results, 
+                      measurevar="Relatedness", 
+                      groupvars=c("X0","Xh","Xn","K1","Bias"))
+
+sumX0_R <- summarySE(results, 
+                     measurevar="Relatedness", 
+                     groupvars=c("X0"))
+
+sumXh_R <- summarySE(results, 
+                     measurevar="Relatedness", 
+                     groupvars=c("Xh"))
+
+sumXn_R <- summarySE(results, 
+                     measurevar="Relatedness", 
+                     groupvars=c("Xn"))
+
+sumK1_R <- summarySE(results, 
+                     measurevar="Relatedness", 
+                     groupvars=c("K1"))
+
+sumBias_R <- summarySE(results, 
+                       measurevar="Relatedness", 
+                       groupvars=c("Bias"))
+
+sumAll_R
+sumX0_R
+sumXh_R
+sumXn_R
+sumK1_R
+sumBias_R
+
 
 
 #Interaction plot using summary statistics
@@ -31,11 +128,26 @@ library("ggpubr")
 ggboxplot(results, x = "Xn", y = "Help", color = "X0",
           palette = c("#00AFBB", "#E7B800"))
 
+ggboxplot(results, x = "Xn", y = "Help", color = "Xh",
+          palette = c("#00AFBB", "#E7B800"))
+
+ggboxplot(results, x = "Xn", y = "Help", color = "K1",
+          palette = c("#00AFBB", "#E7B800"))
+
+ggboxplot(results, x = "Xn", y = "Help", color = "Bias",
+          palette = c("#00AFBB", "#E7B800"))
+
 
 #ANOVA
 
-results_ANOVA <- aov(Help ~ X0 + Xh + Xn + K1 + Bias, data = results)
-summary(results_ANOVA)
+results_ANOVA_Help <- aov(Help ~ X0 + Xh + Xn + K1 + Bias, data = results)
+summary(results_ANOVA_Help)
+
+results_ANOVA_Dispersal <- aov(Dispersal ~ X0 + Xh + Xn + K1 + Bias, data = results)
+summary(results_ANOVA_Dispersal)
+
+results_ANOVA_Relatedness <- aov(Relatedness ~ X0 + Xh + Xn + K1 + Bias, data = results)
+summary(results_ANOVA_Relatedness)
 
 #Tukey multiple pairwise-comparisons
 
