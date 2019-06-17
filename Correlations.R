f9ef7b44c4674bfe6b859dd3305afc3524b61dce
IreneGR92 <42998147+IreneGR92@users.noreply.github.com>
2019-06-17 08:19:04 +0200

    added t test t correlations file

--- Correlations.R
+++ Correlations.R
@@ -3,6 +3,51 @@ getwd()
 #setwd('H:\\PhD\\CODE\\All_results\\Excel_files')
 setwd('C:\\Users\\igaru\\Documents\\PhD\\CODE\\All_results\\Excel_files')
 results<-read.table("SEM.csv",header = TRUE, sep=",")
+names(results)[names(results) == 'ï..Replica'] <- 'Replica'
+
+
+results$X0<-as.factor(results$X0)
+results$Xh<-as.factor(results$Xh)
+results$Xn<-as.ordered(results$Xn)
+results$K1<-as.factor(results$K1)
+results$Bias<-as.factor(results$Bias)
+str(results)
+
+
+#sub<-subset(results, Help >1.75)
+#View(sub)
+
+
+######################################### T TEST  #####################################################3
+
+t.test(results$Help~results$Bias)
+t.test(results$Dispersal~results$Bias)
+t.test(results$Relatedness~results$Bias)
+
+t.test(results$Help~results$X0)
+t.test(results$Dispersal~results$X0)
+t.test(results$Relatedness~results$X0)
+
+t.test(results$Help~results$Xh)
+t.test(results$Dispersal~results$Xh)
+t.test(results$Relatedness~results$Xh)
+
+t.test(results$Help~results$K1)
+t.test(results$Dispersal~results$K1)
+t.test(results$Relatedness~results$K1)
+
+
+res.aov2 <- aov(Help ~ Xn, data = results)
+summary(res.aov2)
+pairwise.t.test(results$Help, results$Xn, p.adjust="none", pool.sd = T) 
+
+res.aov2 <- aov(Dispersal ~ Xn, data = results)
+summary(res.aov2)
+pairwise.t.test(results$Dispersal, results$Xn, p.adjust="none", pool.sd = T) 
+
+res.aov2 <- aov(Relatedness ~ Xn, data = results)
+summary(res.aov2)
+pairwise.t.test(results$Relatedness, results$Xn, p.adjust="none", pool.sd = T) 
 
 
 
@@ -11,6 +56,7 @@ results<-read.table("SEM.csv",header = TRUE, sep=",")
 
 #Function to calculate man and SD per variable per parameter
 library(plyr)
+library(tidyselect)
 
 data_summary <- function(data, varname, groupnames){
   require(plyr)
@@ -38,68 +84,82 @@ library(ggplot2)
 #Help vs Bias
 
 help_summary<-data_summary(results, varname="Help", groupnames=c("Bias"))
-str(help_summary)
 help_summary$se<-SE_formula(help_summary$sd,10)
 
-ggplot(help_summary, aes(x=Bias, y=Help, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+ggplot(help_summary, aes(x=Bias, y=Help, width=0.5)) +
+  geom_bar(stat="identity", position=position_dodge(), fill="red")+
   geom_errorbar(aes(ymin=Help-se, ymax=Help+se), width=0.1,
                 position=position_dodge(0.9))+
   scale_fill_brewer(palette="Paired") + theme_minimal()
+
+
+
+
+help_summary<-data_summary(results, varname="Group_size", groupnames=c("Bias"))
+help_summary$se<-SE_formula(help_summary$sd,10)
+
+ggplot(help_summary, aes(x=Bias, y=Group_size, width=0.5)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  geom_errorbar(aes(ymin=Group_size-se, ymax=Group_size+se), width=0.1,
+                position=position_dodge(0.9))+
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  ylab("Group size")
+
+
 
 
 #Help vs X0
 
 help_summary<-data_summary(results, varname="Help", groupnames=c("X0")) 
-str(help_summary)
 help_summary$se<-SE_formula(help_summary$sd,10)
 
 ggplot(help_summary, aes(x=X0, y=Help, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="red") +
   geom_errorbar(aes(ymin=Help-se, ymax=Help+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Base survival")
 
 
 #Help vs Xh
 
 help_summary<-data_summary(results, varname="Help", groupnames=c("Xh")) 
-str(help_summary)
 help_summary$se<-SE_formula(help_summary$sd,10)
 
 ggplot(help_summary, aes(x=Xh, y=Help, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="red") +
   geom_errorbar(aes(ymin=Help-se, ymax=Help+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Cost help survival")
 
 
 
 #Help vs Xn
 
 help_summary<-data_summary(results, varname="Help", groupnames=c("Xn")) 
-str(help_summary)
 help_summary$se<-SE_formula(help_summary$sd,10)
 
 ggplot(help_summary, aes(x=Xn, y=Help, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="red") +
   geom_errorbar(aes(ymin=Help-se, ymax=Help+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Benefit group size survival")
 
 
 
 #Help vs K1
 
 help_summary<-data_summary(results, varname="Help", groupnames=c("K1")) 
-str(help_summary)
 help_summary$se<-SE_formula(help_summary$sd,10)
 
 ggplot(help_summary, aes(x=K1, y=Help, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="red") +
   geom_errorbar(aes(ymin=Help-se, ymax=Help+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Benefit help fecundity")
 
 
 
@@ -112,7 +172,7 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=Bias, y=Dispersal, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="blue") +
   geom_errorbar(aes(ymin=Dispersal-se, ymax=Dispersal+se), width=0.1,
                 position=position_dodge(0.9))+
   scale_fill_brewer(palette="Paired") + theme_minimal()
@@ -125,10 +185,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=X0, y=Dispersal, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="blue") +
   geom_errorbar(aes(ymin=Dispersal-se, ymax=Dispersal+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Base survival")
 
 
 #Dispersal vs Xh
@@ -138,10 +199,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=Xh, y=Dispersal, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="blue") +
   geom_errorbar(aes(ymin=Dispersal-se, ymax=Dispersal+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Cost help survival")
 
 
 
@@ -152,10 +214,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=Xn, y=Dispersal, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="blue") +
   geom_errorbar(aes(ymin=Dispersal-se, ymax=Dispersal+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Benefit group size survival")
 
 
 
@@ -166,10 +229,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=K1, y=Dispersal, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="blue") +
   geom_errorbar(aes(ymin=Dispersal-se, ymax=Dispersal+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Benefit help fecundity")
 
 
 
@@ -182,7 +246,7 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=Bias, y=Relatedness, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="orange") +
   geom_errorbar(aes(ymin=Relatedness-se, ymax=Relatedness+se), width=0.1,
                 position=position_dodge(0.9))+
   scale_fill_brewer(palette="Paired") + theme_minimal()
@@ -195,10 +259,12 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=X0, y=Relatedness, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="orange") +
   geom_errorbar(aes(ymin=Relatedness-se, ymax=Relatedness+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Base survival")
+
 
 
 #Relatedness vs Xh
@@ -208,10 +274,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=Xh, y=Relatedness, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="orange") +
   geom_errorbar(aes(ymin=Relatedness-se, ymax=Relatedness+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Cost help survival")
 
 
 
@@ -222,10 +289,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=Xn, y=Relatedness, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="orange") +
   geom_errorbar(aes(ymin=Relatedness-se, ymax=Relatedness+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Benefit group size survival")
 
 
 
@@ -236,10 +304,11 @@ str(summary_results)
 summary_results$se<-SE_formula(summary_results$sd,10)
 
 ggplot(summary_results, aes(x=K1, y=Relatedness, width=0.5)) + 
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat="identity", position=position_dodge(), fill="orange") +
   geom_errorbar(aes(ymin=Relatedness-se, ymax=Relatedness+se), width=0.1,
                 position=position_dodge(0.9))+
-  scale_fill_brewer(palette="Paired") + theme_minimal()
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  xlab("Benefit help fecundity")
 
 
 
@@ -258,12 +327,14 @@ library("gridExtra")
 g1<-ggscatter(results, x = "Relatedness", y = "Help", 
               add = "reg.line", conf.int = TRUE, 
               cor.coef = TRUE, cor.method = "spearman",
+              cor.coeff.args = list(label.x = 0.25),
               xlab = "Relatedness", ylab = "Help")
 
 
 g2<-ggscatter(results, x = "Relatedness", y = "Dispersal", 
               add = "reg.line", conf.int = TRUE, 
               cor.coef = TRUE, cor.method = "spearman",
+              cor.coeff.args = list(label.x = 0.25),
               xlab = "Relatedness", ylab = "Dispersal")
 g2<-ggpar(g2, ylim = c(0.0, 1))
 
@@ -271,6 +342,7 @@ g2<-ggpar(g2, ylim = c(0.0, 1))
 g3<-ggscatter(results, x = "Help", y = "Dispersal", 
               add = "reg.line", conf.int = TRUE, 
               cor.coef = TRUE, cor.method = "spearman",
+              cor.coeff.args = list(label.x.npc = "right",label.x = 3, label.y.npc = "bottom"),
               xlab = "Help", ylab = "Dispersal")
 g3<-ggpar(g3, ylim = c(0.0, 1))
 
@@ -278,14 +350,16 @@ g3<-ggpar(g3, ylim = c(0.0, 1))
 g4<-ggscatter(results, x = "Help", y = "Group_size", 
               add = "reg.line", conf.int = TRUE, 
               cor.coef = TRUE, cor.method = "spearman",
+              cor.coeff.args = list(label.x = 3.5),
               xlab = "Help", ylab = "Group size")
 
 
-g5<-ggscatter(results, x = "Group_size", y = "Dispersal", 
+g5<-ggscatter(results, x = "Dispersal", y = "Group_size", 
               add = "reg.line", conf.int = TRUE, 
               cor.coef = TRUE, cor.method = "spearman",
-              xlab = "Group size", ylab = "Dispersal")
-g5<-ggpar(g5, ylim = c(0, 1))
+              cor.coeff.args = list(label.x = 0.6),
+              xlab = "Dispersal", ylab = "Group size")
+g5<-ggpar(g5, xlim = c(0, 1))
 
 
 g6<-ggscatter(results, x = "Relatedness", y = "Group_size", 
@@ -296,8 +370,9 @@ g6<-ggscatter(results, x = "Relatedness", y = "Group_size",
 g7<-ggscatter(results, x = "Relatedness", y = "Help_Disp", 
               add = "reg.line", conf.int = TRUE, 
               cor.coef = TRUE, cor.method = "spearman",
+              cor.coeff.args = list(label.x = 0),
               xlab = "Relatedness", ylab = "Help-Dispersal")
 
 
 
-grid.arrange(g1, g2, g3, g4, g5, g6, g7, nrow = 4)
+grid.arrange(g1, g2, g3, g4, g5, g6, nrow = 2)
